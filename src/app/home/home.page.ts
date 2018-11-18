1f3be33eaadf1e4e847173e9e81b4efef5a2cf61
bastok <bfromager@gmail.com>
2018-11-18 08:13:41 +0100

    Change Point/Cell from tupples to Class & Interface Add Position interface nextCellFromPosition

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {Board, Direction} from '../services/board';
+import {Board, Direction, Point, Position} from '../services/board';
 
 @Component({
   selector: 'app-home',
@@ -9,9 +9,59 @@ import {Board, Direction} from '../services/board';
 export class HomePage {
 
     constructor() {
-        let board: Board = new Board([0,0],Direction.Down);
-        board.setCase(0,0,[false, [1,2]]);
+        let board: Board = new Board({x:0,y:0},Direction.Down);
+        let board2: Board = new Board({x:0,y:0},Direction.Down, board);
+
+        board.getCell(5,0).used = true;
+        board2.getCell(0,5).used = true;
         board.debugBoard();
-        let board2: Board = new Board([0,0],Direction.Down, board);
+        board2.debugBoard();
+
+        let point: Point;
+        let position: Position;
+
+        console.log("-----------------");
+        point = {x:5, y:0};
+        position = {origin: point, direction: Direction.Left};
+        console.log("position : (" + position.origin.x.toString() + "," +  position.origin.y.toString() + ") -> " + position.direction.toString());
+        let result = board.nextCellFromPosition(position);
+        while (result) {
+            let position2: Position = <Position>result;
+            console.log("position2 : (" + position2.origin.x.toString() + "," +  position2.origin.y.toString() + ") -> " + position2.direction.toString());
+            result = board.nextCellFromPosition(position2);
+        }
+
+        console.log("-----------------");
+        point = {x:5, y:1};
+        position = {origin: point, direction: Direction.Left};
+        console.log("position : (" + position.origin.x.toString() + "," +  position.origin.y.toString() + ") -> " + position.direction.toString());
+        result = board.nextCellFromPosition(position);
+        while (result) {
+            let position2: Position = <Position>result;
+            console.log("position2 : (" + position2.origin.x.toString() + "," +  position2.origin.y.toString() + ") -> " + position2.direction.toString());
+            result = board.nextCellFromPosition(position2);
+        }
+
+        console.log("-----------------");
+        point = {x:5, y:2};
+        position = {origin: point, direction: Direction.Left};
+        console.log("position : (" + position.origin.x.toString() + "," +  position.origin.y.toString() + ") -> " + position.direction.toString());
+        result = board.nextCellFromPosition(position);
+        while (result) {
+            let position2: Position = <Position>result;
+            console.log("position2 : (" + position2.origin.x.toString() + "," +  position2.origin.y.toString() + ") -> " + position2.direction.toString());
+            result = board.nextCellFromPosition(position2);
+        }
+
+        console.log("-----------------");
+        point = {x:0, y:5};
+        position = {origin: point, direction: Direction.Up};
+        console.log("position : (" + position.origin.x.toString() + "," +  position.origin.y.toString() + ") -> " + position.direction.toString());
+        result = board.nextCellFromPosition(position);
+        while (result) {
+            let position2: Position = <Position>result;
+            console.log("position2 : (" + position2.origin.x.toString() + "," +  position2.origin.y.toString() + ") -> " + position2.direction.toString());
+            result = board.nextCellFromPosition(position2);
+        }
     }
 }
--- src/app/services/board.ts
+++ src/app/services/board.ts
@@ -11,62 +11,143 @@ export const enum Direction {
     Left = 3,
 }
 
-export type Point = [number,number];
-export type Case = [boolean,number[]];
+export interface Point {
+    x : number;
+    y : number;
+}
+
+export interface Position {
+    origin : Point;
+    direction : Direction;
+}
+
+export class Cell {
+    used: boolean = false;
+    pass = new Set();
+
+    constructor(used? : boolean) {
+        if (used) this.used = used;
+    }
+
+    toString(): string {
+        return this.used ? 'X' : '.' ;
+    }
+
+    copy(): Cell {
+        return new Cell(this.used);
+    }
+} ;
 
 const SIZE =6;
 
 export class Board {
     // public direction: Direction = Direction.Up;
-    private _freeCases: Matrix<Case> = [];
+    private _Cells: Matrix<Cell> = [];
 
 
     constructor(public position: Point, public direction: Direction, fromBoard?: Board) {
 
-        if (fromBoard) {
-            this.copyBoard(fromBoard);
-        } else {
-            for (let row = 0; row < SIZE; ++row) {
-                this._freeCases[row] = [];
-                for (let col = 0; col < SIZE; ++col) {
-                    this._freeCases[row][col] = [true, []];
-                }
+        for (let row = 0; row < SIZE; ++row) {
+            this._Cells[row] = [];
+            for (let col = 0; col < SIZE; ++col) {
+                this._Cells[row][col] = fromBoard ? fromBoard.getCell(col,row).copy() : new Cell();
             }
-
-            // this._freeCases[0][0] = [false,[]];
-            // this._freeCases[2][3] = [false,[]];
-            this.setCase(0, 0, [false, []]);
-            this.setCase(3, 2, [false, []]);
         }
-        this.debugBoard();
-    }
 
-    debugCase(cell: Case) : string {
-        return cell[0].toString();
+        this.getCell(0,0).used = true;
+        this.getCell(3,2).used = true;
+
+        // this.debugBoard();
     }
 
     debugBoard(){
-        debugMatrix(this._freeCases,this.debugCase);
+        debugMatrix(this._Cells);
     }
 
-    getCase(x: number,y: number): Case {
-        return this._freeCases[y][x];
-    }
-    setCase(x: number,y: number, _case: Case){
-        this._freeCases[y][x] = _case;
+    getCell(col: number,row: number): Cell {
+        return this._Cells[row][col];
     }
 
-    copyBoard(fromBoard: Board) {
-        for (let row=0; row < SIZE; ++row) {
-            this._freeCases[row] = [];
-            for (let col=0; col < SIZE; ++col) {
-                this._freeCases[row][col] = [true, []];
-                this._freeCases[row][col][0] = false;
-                this._freeCases[row][col][1] = [2,3];
-                // this._freeCases[row][col][0] = fromBoard[row][col][0];
-                // this._freeCases[row][col][1] = [...fromBoard[row][col][1]];
-                // this._freeCases[row][col][1] = fromBoard[row][col][1].slice();
+    nextCellFromPosition(position: Position): Position | boolean {
+        let point: Point = <Point>{x: position.origin.x, y: position.origin.y};
+        let result: Position = <Position>{origin: point, direction: position.direction};
+
+        // Cell 0,0
+        if (position.origin.x == 1 && position.origin.y == 0 && position.direction == Direction.Left) {
+            result.origin.x = 0;
+            result.origin.y = 1;
+            result.direction = Direction.Down;
+        } else
+        if (position.origin.x == 0 && position.origin.y == 1 && position.direction == Direction.Up) {
+            result.origin.x = 1;
+            result.origin.y = 0;
+            result.direction = Direction.Right;
+        } else
+
+        // Cell 3,2
+        if (position.origin.x == 3 && position.origin.y == 1 && position.direction == Direction.Down) {
+            result.origin.x = 2;
+            result.origin.y = 2;
+            result.direction = Direction.Left;
+        } else
+        if (position.origin.x == 2 && position.origin.y == 2 && position.direction == Direction.Right) {
+            result.origin.x = 3;
+            result.origin.y = 1;
+            result.direction = Direction.Up;
+        } else
+        if (position.origin.x == 4 && position.origin.y == 2 && position.direction == Direction.Left) {
+            result.origin.x = 3;
+            result.origin.y = 3;
+            result.direction = Direction.Down;
+        } else
+        if (position.origin.x == 3 && position.origin.y == 3 && position.direction == Direction.Up) {
+            result.origin.x = 4;
+            result.origin.y = 2;
+            result.direction = Direction.Right;
+        } else
+
+        switch (position.direction) {
+            case Direction.Up : {
+                -- result.origin.y;
+                break;
             }
+            case Direction.Down : {
+                ++ result.origin.y;
+                break;
+            }
+            case Direction.Left : {
+                -- result.origin.x;
+                break;
+            }
+            case Direction.Right : {
+                ++ result.origin.x;
+                break;
+            }
+            default :
+                return false;
         }
+
+        if ((result.origin.x < 0) || (result.origin.y < 0) || (result.origin.x >= SIZE) || (result.origin.y >= SIZE))
+            return false;
+        else
+            return result;
     }
+
+    // setCell(x: number,y: number, cell: Cell){
+    //     this._Cells[y][x] = cell;
+    // }
+
+    // copyBoard(fromBoard: Board) {
+    //     for (let row=0; row < SIZE; ++row) {
+    //         this._Cells[row] = [];
+    //         for (let col=0; col < SIZE; ++col) {
+    //             this._Cells[row][col] = [true, []];
+    //             this._Cells[row][col][0] = false;
+    //             this._Cells[row][col][1] = [2,3];
+    //             // this._freeCases[row][col][0] = fromBoard[row][col][0];
+    //             // this._freeCases[row][col][1] = [...fromBoard[row][col][1]];
+    //             // this._freeCases[row][col][1] = fromBoard[row][col][1].slice();
+    //         }
+    //     }
+    // }
 }
